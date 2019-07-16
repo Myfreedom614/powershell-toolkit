8b8c41940f7aba2c8b31540621f788e4b4e02472
Franklin Chen <myfreedom614@gmail.com>
2019-07-16 17:37:21 +0800

    Create flv-to-mp4.ps1

---
+++ flv-to-mp4.ps1
@@ -0,0 +1,13 @@
+﻿$viddir = ls -filter *.flv
+foreach ($video in $viddir)
+{
+  $newname = ([io.path]::GetFileNameWithoutExtension($video))
+  IF (-Not (Test-Path "$newname.mp4") -Or (Get-Item "$newname.mp4").length -eq 0kb){
+      ffmpeg.exe -i $video -c copy "$newname.mp4"
+      IF (Test-Path "$newname.mp4") {
+        If ((Get-Item "$newname.mp4").length -gt 0kb) {
+          Remove-Item $video
+        }
+      }
+  }
+}
